4a27abf4790a0f6ea9c60d46820331836d0575e7
RaviTeja1B <105812470+RaviTeja1B@users.noreply.github.com>
2022-10-26 23:24:46 +0530

    Update index.js

--- cat_on_off/index.js
+++ cat_on_off/index.js
@@ -2,9 +2,6 @@ function onSwitch_1(){
     document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
     document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
     document.getElementById("switchStatus").textContent = "Switched Off";
-    
-
-    
     document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
     document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
 }
